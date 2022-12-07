132b55518fa833a6ec29fc459b03787762b52078
Rahul <rahulgrover4841@gmail.com>
2022-12-07 13:48:22 +0530

    added some more contents

--- index.js
+++ index.js
@@ -8,5 +8,16 @@ const options = {
 
 fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=Delhi', options)
 	.then(response => response.json())
-	.then(response => console.log(response))
+	.then((response) => {
+        console.log(response)
+        // cloud_pct.innerHTML= response.cloud_pct
+        feels_like.innerHTML= response.feels_like
+        humidity.innerHTML= response.humidity
+        max_temp.innerHTML= response.max_temp
+        min_temp.innerHTML= response.min_temp
+        sunrise.innerHTML= response.sunrise
+        sunset.innerHTML= response.sunset
+        temp.innerHTML= response.temp
+        wind_degrees.innerHTML= response.wind_degrees
+    })
 	.catch(err => console.error(err));
